1adc0d263d93fc89c07511184c2ef95d1687c744
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-05-24 02:41:00 -0700

    IGW Updated

--- js/app.js
+++ js/app.js
@@ -27,3 +27,92 @@ function scrollFunction() {
     document.getElementById("webname_wrapper").style.width = "30%";
   }
 }
+
+
+// Activate Link Functions
+
+function homeLink() {
+  document.getElementById('home').style.borderBottom = '2px solid rgb(0, 48, 0)';
+  document.getElementById('home').style.color = '#858585';
+
+  document.getElementById('products').style.border = 'none';
+  document.getElementById('services').style.border = 'none';
+  document.getElementById('about').style.border = 'none';
+  document.getElementById('contact').style.border = 'none';
+
+  document.getElementById('products').style.color = '#000';
+  document.getElementById('services').style.color = '#000';
+  document.getElementById('about').style.color = '#000';
+  document.getElementById('contact').style.color = '#000';
+}
+
+function productsLink() {
+  document.getElementById('products').style.borderBottom = '2px solid rgb(0, 48, 0)';
+  document.getElementById('products').style.color = '#858585';
+
+  document.getElementById('home').style.border = 'none';
+  document.getElementById('services').style.border = 'none';
+  document.getElementById('about').style.border = 'none';
+  document.getElementById('contact').style.border = 'none';
+
+  document.getElementById('home').style.color = '#000';
+  document.getElementById('services').style.color = '#000';
+  document.getElementById('about').style.color = '#000';
+  document.getElementById('contact').style.color = '#000';
+}
+
+function servicesLink() {
+  document.getElementById('services').style.borderBottom = '2px solid rgb(0, 48, 0)';
+  document.getElementById('services').style.color = '#858585';
+
+  document.getElementById('products').style.border = 'none';
+  document.getElementById('home').style.border = 'none';
+  document.getElementById('about').style.border = 'none';
+  document.getElementById('contact').style.border = 'none';
+
+  document.getElementById('home').style.color = '#000';
+  document.getElementById('products').style.color = '#000';
+  document.getElementById('about').style.color = '#000';
+  document.getElementById('contact').style.color = '#000';
+}
+
+function aboutLink() {
+  document.getElementById('about').style.borderBottom = '2px solid rgb(0, 48, 0)';
+  document.getElementById('about').style.color = '#858585';
+
+  document.getElementById('products').style.border = 'none';
+  document.getElementById('services').style.border = 'none';
+  document.getElementById('home').style.border = 'none';
+  document.getElementById('contact').style.border = 'none';
+
+  document.getElementById('home').style.color = '#000';
+  document.getElementById('services').style.color = '#000';
+  document.getElementById('products').style.color = '#000';
+  document.getElementById('contact').style.color = '#000';
+}
+
+function contactLink() {
+  document.getElementById('contact').style.borderBottom = '2px solid rgb(0, 48, 0)';
+  document.getElementById('contact').style.color = '#858585';
+
+  document.getElementById('products').style.border = 'none';
+  document.getElementById('services').style.border = 'none';
+  document.getElementById('about').style.border = 'none';
+  document.getElementById('home').style.border = 'none';
+
+  document.getElementById('home').style.color = '#000';
+  document.getElementById('services').style.color = '#000';
+  document.getElementById('about').style.color = '#000';
+  document.getElementById('products').style.color = '#000';
+}
+
+// Hover Effect 
+function hoverEffect(id) {
+  const getId = document.getElementById(id)
+  getId = 'hover';
+}
+
+// function leaveEffect(id) {
+//   document.getElementById(id).style.border = 'none';
+//   document.getElementById(id).style.color = '#000';
+// }
